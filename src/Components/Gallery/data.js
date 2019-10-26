b09450333dab9e6f072cedf41f9ce92c873f6e49
KrzGas <kgasik@gmail.com>
2019-10-26 11:14:55 +0200

    photo fix 2

--- src/Components/Gallery/data.js
+++ src/Components/Gallery/data.js
@@ -5,16 +5,16 @@ import woman2 from "../../../img/woman2.jpg";
 import woman3 from "../../../img/woman3.jpg";
 
 const data = [
-    {name: "Basia Sołtyńska", title: "Founder",localization: "Warsaw", image: man1},
-    {name: "Włas Chorowiec", title: "Founder",localization: "Warsaw", image: woman1},
-    {name: "Edyta Leśniewska", title: "Head of Human Resources",localization: "Warsaw", image: man2},
-    {name: "Joanna Pawluk", title: "Chief Growth Officer (CGO)",localization: "London", image: woman2},
-    {name: "David Saunders", title: "Business Development",localization: "London", image: woman3},
-    {name: "Adam Elridge", title: "Business Development Director US",localization: "Los Angeles", image: woman3},
-    {name: "Maha Mahda", title: "Chief Business Development Officer (CBDO)",localization: "Warsaw", image: man1},
-    {name: "Elżbieta Kamińska", title: "International Client Service Manager",localization: "Warsaw", image: woman1},
+    {name: "Basia Sołtyńska", title: "Founder",localization: "Warsaw", image: woman1},
+    {name: "Włas Chorowiec", title: "Founder",localization: "Warsaw", image: man1},
+    {name: "Edyta Leśniewska", title: "Head of Human Resources",localization: "Warsaw", image: woman2},
+    {name: "Joanna Pawluk", title: "Chief Growth Officer (CGO)",localization: "London", image: woman3},
+    {name: "David Saunders", title: "Business Development",localization: "London", image: man2},
+    {name: "Adam Elridge", title: "Business Development Director US",localization: "Los Angeles", image: man2},
+    {name: "Maha Mahda", title: "Chief Business Development Officer (CBDO)",localization: "Warsaw", image: woman1},
+    {name: "Elżbieta Kamińska", title: "International Client Service Manager",localization: "Warsaw", image: woman3},
     {name: "Karolina Makuch", title: "Creation Manager",localization: "Warsaw", image: woman2},
-    {name: "Kamil Bolek", title: "Founder",localization: "Warsaw", image: man2}
+    {name: "Kamil Bolek", title: "Founder",localization: "Warsaw", image: man1}
 ];
 
 export {data};
